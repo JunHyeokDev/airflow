67fa5c9c0d3ec53bb2b3deadb930d3a34b79c513
Jun <fixme1537@gmail.com>
2026-02-13 17:46:16 +0100

    no_args

---
+++ dags/dags_python_with_no_args.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
+#from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from common.common_func import regist
+
+
+with DAG(
+    dag_id="dags_python_with_no_args",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2023, 1, 1, tz="Europe/Warsaw"),
+    catchup=False,
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id="regist_t1",
+        python_callable=regist,
+        op_args=["홍길동", "남자", "옵션1", "옵션2"],
+    )
+    
+    regist_t1
+    
