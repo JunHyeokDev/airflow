1edc2e52e6e3c9ffd6b2b5f7f1e60d68a9352046
Jun <fixme1537@gmail.com>
2026-02-14 10:35:52 +0100

    retryyyyㅛ

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -7,13 +7,14 @@ def regist(name, sex, *args):
     print(f'기타옵션들: {args}')
     
 def regist_kwargs(**kwargs):
-    # kwargs.values()를 리스트로 바꿔서 0번, 1번, 그리고 나머지[2:]로 분리
-    vals = list(kwargs.values())
+    # Airflow가 넣어준 수많은 정보 중 내가 보낸 것만 꺼냅니다.
+    # 인자가 없을 경우를 대비해 기본값(None)을 설정하면 안전합니다.
+    name = kwargs.get("name", "이름없음")
+    sex = kwargs.get("sex", "알수없음")
     
-    name = vals[0]
-    sex = vals[1]
-    args = vals[2:] # 2번째 인덱스부터 끝까지 (리스트처럼 동작)
+    # 전달받은 특정 옵션들만 리스트로 묶고 싶다면 직접 지정하세요.
+    options = [kwargs.get("option1"), kwargs.get("option2")]
 
     print(f'이름: {name}')
     print(f'성별: {sex}')
-    print(f'기타옵션들: {args}')
+    print(f'기타옵션들: {options}')
