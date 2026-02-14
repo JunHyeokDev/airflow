856590b43b0d9466b6a3a0f4d984fce81828a309
Jun <fixme1537@gmail.com>
2026-02-14 10:34:06 +0100

    retryyyy

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -7,8 +7,13 @@ def regist(name, sex, *args):
     print(f'기타옵션들: {args}')
     
 def regist_kwargs(**kwargs):
-# 1. 값들만 리스트로 변환: list(kwargs.values())
-    # 2. 첫 번째 이후부터 슬라이싱: [1:]
-    other_options = list(kwargs.values())[1:]
-    print(f'첫 번째 값(이름): {list(kwargs.values())[0]}')
-    print(f'나머지 모든 값들: {other_options}')
+    # kwargs.values()를 리스트로 바꿔서 0번, 1번, 그리고 나머지[2:]로 분리
+    vals = list(kwargs.values())
+    
+    name = vals[0]
+    sex = vals[1]
+    args = vals[2:] # 2번째 인덱스부터 끝까지 (리스트처럼 동작)
+
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    print(f'기타옵션들: {args}')
