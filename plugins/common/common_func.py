e4c3c41387e778f9e0bd09762551dbb39ed09eaa
Jun <fixme1537@gmail.com>
2026-02-14 10:29:09 +0100

    kwargs prints

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,3 +5,10 @@ def regist(name, sex, *args):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타옵션들: {args}')
+    
+def regist_kwargs(**kwargs):
+# 1. 값들만 리스트로 변환: list(kwargs.values())
+    # 2. 첫 번째 이후부터 슬라이싱: [1:]
+    other_options = list(kwargs.values())[1:]
+    print(f'첫 번째 값(이름): {list(kwargs.values())[0]}')
+    print(f'나머지 모든 값들: {other_options}')
